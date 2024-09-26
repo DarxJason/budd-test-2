92d00ad9403bab651490fd8cb0525cf53e5c5bd5
DarxJason <165398554+DarxJason@users.noreply.github.com>
2024-09-25 20:55:30 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -110,7 +110,7 @@ export class mainMap extends Phaser.Scene {
                 fill: '#0f0'
             })
             .setInteractive()
-            .on('pointerdown', createAccount);
+            .on('pointerdown', createAccount());
         // Button to login
         const loginButton = this.add.text(650, 50, 'Login', {
                 fill: '#00f'
